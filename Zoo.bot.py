a595dafac571f61e93c7895646075fc108f465b6
Kreyzdr <kilka130606@gmail.com>
2025-01-01 02:47:07 +0300

    Показывает нужный ответ

--- Zoo.bot.py
+++ Zoo.bot.py
@@ -1,17 +1,19 @@
-import telebot
+import telebot # импортируем основную библиотеку
 from telebot import types
 
-from sekret.key import TOKEN
-from animals_and_their_characteristics import animals
-from questions import questions
+from sekret.key import TOKEN # импортируем токен
+from animals_and_their_characteristics import animals # импортируем данные (я бы назвал дерево), о животных
+from questions import questions # импортируем вопросы
 
 bot = telebot.TeleBot(TOKEN)
 
+# для id сообщение, дабы потом его удалить
 last_message_id = None
 
+# словарь где id пользователя будет указывать на словарь с current_question - счётчик вопросов, answers - список ответов
 user_states = {}
 
-
+# словарь начальных команд(планирую засунуть в файл с вопросоми или с животными), для клавы
 commands = {
     "start": "Начать диолог",
     "help": "Помощь"
@@ -20,6 +22,10 @@ commands = {
 
 @bot.message_handler(commands=["help", 'start'])
 def initial_message(massege):
+    """
+    Первые сообщение информирующие о запуске бота и краткая информация о боте.
+    Так же запускает кнопки команд, клава находится у сообщения.
+    """
 
     markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
     for cmd in commands:
@@ -33,10 +39,14 @@ def initial_message(massege):
 
 @bot.message_handler(commands=["values"])
 def start_questions(massage):
+    """Кориктируем словарь"""
     user_states[massage.chat.id] = {"current_question": 0, "answers": []}
     ask_question(massage.chat.id)
 
 def ask_question(chat_id):
+    """Основная функция.
+    Задаюм вопросы
+    """
     state = user_states[chat_id]
     question_keys = list(questions.keys())
 
@@ -64,16 +74,15 @@ def ask_question(chat_id):
     else:
         show_animal(chat_id)
 
-print(last_message_id)
 @bot.callback_query_handler(func=lambda call: True)
 def handle_query(call):
+    """Делаем так чтобы вопросы по очереди задавались"""
     chat_id = call.message.chat.id
     user_state = user_states[chat_id]
 
     # Сохраняем ответ
     user_state["answers"].append(call.data)
 
-    bot.delete_message(call.message.chat.id, last_message_id)
 
     # Переходим к следующему вопросу
     user_state["current_question"] += 1
@@ -82,6 +91,7 @@ def handle_query(call):
 
 
 def show_animal(chat_id):
+    """Результаты"""
     user_state = user_states[chat_id]
     do_result = user_state["answers"]
     n = 0
